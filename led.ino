aaa76ceec87ca1d792911fcf416dbb6b7b282036
ATWIINETIRZAH <117368965+ATWIINETIRZAH@users.noreply.github.com>
2023-02-20 14:45:39 -0600

    Add files via upload
    blinking led

---
+++ led.ino
@@ -0,0 +1,14 @@
+
+void
+setup()
+{
+pinMode(9, OUTPUT);// initialize digital pin3 as an output.
+}
+void loop() // the loop function runs over and over again forever
+{ digitalWrite(9, HIGH); // turn the LED on (HIGH is the voltage level)
+delay(1000); // wait for a second
+digitalWrite(9, LOW); // turn the LED off by making the voltage LOW
+
+
+delay(1000); // wait for a second
+}//*******************************************************************
